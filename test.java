14718879ff66ab630a77694089d9aac52174f568
ChristianBurke03 <christian.burke@maine.edu>
2024-09-16 15:45:00 -0400

    Update test.java

--- test.java
+++ test.java
@@ -8,6 +8,19 @@ public class test {
 		Airport apl = new Airport("Delta", "York", "Maine");
 		System.out.println(apl.getName()+", "+apl.getCity()+", "+apl.getState());
 		System.out.println(args[0]);
-}
 
+		// Read the flight data from a file
+		Flight[] flights = myDataReader.readDataFile(args[0]);
+
+		// Get airports in Maine using MyAnalyzer
+			Set<String> maineAirports = MyAnalyzer.getAirportsInMaine(flights);
+       		 // Print all the airport names
+			System.out.println("Airports in Maine:");
+			for (String airport : maineAirports) {
+				System.out.println(airport);
+			}
+
+		// Part B
+    		int maxPassengersToPWM = MyAnalyzer.getMaxPassengersToPWM(flights);
+    		System.out.println("The max passengers to PWM is, " + maxPassengersToPWM);
 }
