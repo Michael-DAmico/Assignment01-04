c08ab6ebfa4326c507fcbf7a24dae35c4ac4468a
Michael <michael.damico@maine.edu>
2024-10-08 22:40:31 -0400

    Update MyQueue.java

--- MyQueue.java
+++ MyQueue.java
@@ -1,40 +1,42 @@
+/**
+ * @author Christian Burke and Michael D'Amico
+ * @version 8 October 2024
+ */
 package flightpack;
 
 import java.util.ArrayList;
 
 public class MyQueue<T> {
 	
-	ArrayList<T> queue;
-	
-	public MyQueue() {
-		queue = new ArrayList<T>();	
-		
-	}
-		
-	public boolean offer(T input) {
-		return queue.add(input);
-	}
-	
-	public T poll() {
-		if(queue.size()==0)
-			throw new NullPointerException();
-		
-		T item = queue.remove(0);
-		return item;
-		
-	}
-	
-	public boolean isEmpty() {
-		
-	}
-	public int size() {
-		
-		
-	}
-	
-	public T peek() {
-		
-	}
-	
+    ArrayList<T> queue;
 
+    public MyQueue() {
+        queue = new ArrayList<>();
+    }
+
+    public boolean offer(T input) {
+        return queue.add(input);
+    }
+
+    public T poll() {
+        if (queue.isEmpty()) {
+            throw new NullPointerException("Queue is empty");
+        }
+        return queue.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NullPointerException("Queue is empty");
+        }
+        return queue.get(0);
+    }
 }
